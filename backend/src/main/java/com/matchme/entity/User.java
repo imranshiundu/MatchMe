375278f65a0ba1896916af1cdbf4a990d39ea650
Lauri Musto <laurimusto@gmail.com>
2026-03-19 14:12:18 +0200

    user entity

---
+++ backend/src/main/java/com/matchme/entity/User.java
@@ -0,0 +1,26 @@
+package com.matchme.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    private String location;
+
+}
