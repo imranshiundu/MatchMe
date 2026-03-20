c493f76a4f6947248988e6ead6a25e634e362b8a
Lauri Musto <laurimusto@gmail.com>
2026-03-20 12:35:08 +0200

    simple findAll() for Profile and UserService

--- backend/src/main/java/com/matchme/service/ProfileService.java
+++ backend/src/main/java/com/matchme/service/ProfileService.java
@@ -1,4 +1,20 @@
 package com.matchme.service;
 
+import com.matchme.entity.Profile;
+import com.matchme.repository.ProfileRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProfileService {
+    private final ProfileRepository profileRepository;
+
+    public ProfileService(ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
+
+    public List<Profile> findAll(){
+        return profileRepository.findAll();
+    }
 }
--- backend/src/main/java/com/matchme/service/UserService.java
+++ backend/src/main/java/com/matchme/service/UserService.java
@@ -1,4 +1,21 @@
 package com.matchme.service;
 
+import com.matchme.entity.User;
+import com.matchme.repository.UserRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll(User user) {
+        return userRepository.findAll();
+    }
 }
