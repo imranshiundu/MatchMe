4d6833e9e1ad8e96af34b66b1fc6eb9ea8f95852
Lauri Musto <laurimusto@gmail.com>
2026-03-22 20:42:33 +0200

    jwt token generation

--- backend/src/main/java/com/backend/matchme/service/AuthService.java
+++ backend/src/main/java/com/backend/matchme/service/AuthService.java
@@ -4,18 +4,28 @@ import com.backend.matchme.dto.LoginResponseDTO;
 import com.backend.matchme.entity.User;
 import com.backend.matchme.exception.InvalidCredentialsException;
 import com.backend.matchme.repository.UserRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+
+import static io.jsonwebtoken.SignatureAlgorithm.HS256;
 
 @Service
 public class AuthService {
+
     private final UserService userService;
     private final LoginService loginService;
     private final UserRepository userRepository;
     @Value("${jwt.secret}")
     private String jwtSecret;
+    SecretKey secretKey = Keys.hmacShaKeyFor(jwtSecret.getBytes());
     @Value("${jwt.expiration}")
     private Long jwtExpirationMs;
 
@@ -28,7 +38,14 @@ public class AuthService {
     public LoginResponseDTO login(String email, String password) {
         if (loginService.authenticate(email, password)) {
             User user = userRepository.findByEmail(email).orElseThrow(() -> new InvalidCredentialsException("User not found"));
-
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("userId", user.getId());
+            claims.put("email", user.getEmail());
+            Date now = new Date();
+            Date expiryDate = new Date(now.getTime() + jwtExpirationMs);
+            String token = Jwts.builder()
+                    .setClaims(claims).setIssuedAt(now).setExpiration(expiryDate).signWith(secretKey, HS256).compact();
+            return new LoginResponseDTO(token, user.getEmail(), user.getId());
         }
 
         return null;
