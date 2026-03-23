0246c0192a3fc1dfb522a94ff54b75f9252306ea
Lauri Musto <laurimusto@gmail.com>
2026-03-23 06:52:48 +0200

    JWTFilter override

--- backend/src/main/java/com/backend/matchme/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/backend/matchme/security/JwtAuthenticationFilter.java
@@ -1,6 +1,26 @@
 package com.backend.matchme.security;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+/* What happens at runtime
+ * Request comes in
+ * Spring Security hits your filter
+ * Your method runs
+ * YOU  read JWT
+ *      validate
+ *      set authentication
+ * Pass request forward
+ * */
+
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
 }
