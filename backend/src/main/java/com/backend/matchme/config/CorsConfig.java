c9a4c29db5e3e978af214c0f9ab554c0da8fa406
Lauri Musto <laurimusto@gmail.com>
2026-03-28 12:42:48 +0200

    CorsConfig now accepts requests from React frontend:5174

--- backend/src/main/java/com/backend/matchme/config/CorsConfig.java
+++ backend/src/main/java/com/backend/matchme/config/CorsConfig.java
@@ -1,4 +1,24 @@
 package com.backend.matchme.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() { //Spring MVC is the part that handles HTTP requests.
+            @Override //we override because we want to use these settings instead of default ones.
+            public void addCorsMappings(CorsRegistry corsRegistry) {
+                corsRegistry.addMapping("/**") //applies to all endpoints.
+                        .allowedOrigins("http://localhost:5174") //this indicates react dev server.
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // indicates which methods are allowed.
+                        .allowedHeaders("*") // indicates that all headers are allowed.
+                        .allowCredentials(true); // this is required if using Authorization header.
+            }
+        };
+    }
+
 }
