766bbf9faca2acba4af649a3083108018d8a8f6e
Lauri Musto <laurimusto@gmail.com>
2026-04-08 18:34:20 +0300

    editing profile via /me/editProfile is now possible

--- backend/src/main/java/com/backend/matchme/controller/ProfileController.java
+++ backend/src/main/java/com/backend/matchme/controller/ProfileController.java
@@ -1,5 +1,6 @@
 package com.backend.matchme.controller;
 
+import com.backend.matchme.dto.EditProfileDTO;
 import com.backend.matchme.dto.ProfilePostDTO;
 import com.backend.matchme.dto.ProfileResponseDTO;
 import com.backend.matchme.repository.ProfileRepository;
@@ -19,16 +20,25 @@ public class ProfileController {
         this.profileService = profileService;
 
     }
+
     // /me: which is a shortcut to /users/{id} for the authenticated user. You should also implement /me/profile and /me/bio.
     @GetMapping({"/me", "/me/profile", "/me/bio"}) //gets your own authorized profile
     public ProfileResponseDTO getProfile() throws AccessDeniedException {
         return profileService.getProfile();
     }
+
     //TODO: returns the user's name and link to the profile picture.
     //TODO: If the id is not found, or the user does not have permission to view that profile, it must return HTTP404.
     @GetMapping("/profile/{id}")
     public ProfileResponseDTO getProfile(@PathVariable long id) throws AccessDeniedException {
         return profileService.getProfileWithId(id);
+    }
+
+    @PatchMapping("/me/editProfile")
+    public ProfileResponseDTO editProfile(@RequestBody EditProfileDTO editProfileDTO) throws AccessDeniedException {
+        return profileService.editProfile(editProfileDTO);
+
+
     }
 
 }
