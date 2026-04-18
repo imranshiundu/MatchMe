711abd9db9a9c003c78c470e5e06abc7b75717e0
imranshiundu <imranshiundu@gmail.com>
2026-04-18 18:07:34 +0300

    chore: trim security config comments

--- backend/src/main/java/com/backend/matchme/config/SecurityConfig.java
+++ backend/src/main/java/com/backend/matchme/config/SecurityConfig.java
@@ -19,32 +19,23 @@ public class SecurityConfig {
     @Autowired
     private JwtAuthenticationFilter jwtFilter;
 
-    //disable security features:
-    //permitAll lets you hit any hitpoint without auth
-    //csrf.disable makes it possible to send POST/PUT without csrf token
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         http
-                .csrf(AbstractHttpConfigurer::disable)// disable CSRF = Cross-Site Request Forgery. not needed when we use JWT because we are stateless.
-                .formLogin(AbstractHttpConfigurer::disable) //don't redirect to /login because react handles it. Enables server side login flow.
-                .httpBasic(AbstractHttpConfigurer::disable) //disable http basic authentication
-                .logout(AbstractHttpConfigurer::disable) //backend is stateless and doesn't track sessions
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //no memory of login on the server between requests.
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth.
-                        //TODO: remove /users from allowed endpoints later on.
-                                requestMatchers("/login", "/register", "/users", "/ws-chat/**") //these endpoints are allowed without authentication
+                                requestMatchers("/login", "/register", "/users", "/ws-chat/**")
                         .permitAll()
-                        .anyRequest()//anything that comes after those, needs authentication token.
+                        .anyRequest()
                         .authenticated());
-        //              .permitAll() to enable every endpoint without securing for testing purposes
-        //              .authenticated() to secure all endpoints.
 
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
-
-
 }
