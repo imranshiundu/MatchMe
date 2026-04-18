172f2280c605e517f6685f1ba07c7ebc99434183
imranshiundu <imranshiundu@gmail.com>
2026-04-18 18:15:52 +0300

    chore: remove frontend from repo

--- frontend/src/components/chat/ChatBubble.tsx
+++
@@ -1,9 +0,0 @@
-function ChatBubble({message, fromSender}) {
-    return (
-        <div
-            className={`${fromSender ? 'max-w-1/2 bg-[#D8FF80] text-[#141F00] ml-auto mr-5 my-5 px-5 py-2 rounded-xl rounded-br-none' : 'max-w-1/2 bg-[#1c1b1b] mr-auto ml-5 my-5 px-5 py-2 rounded-xl rounded-tl-none'}`}
-        >{message}</div>
-    )
-}
-
-export default ChatBubble;
