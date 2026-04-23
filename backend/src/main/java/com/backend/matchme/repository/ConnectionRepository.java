29b790b8eeb1ed3d6ee742b9e67a1dc4e1bfacea
Lauri Musto <Laurimusto@gmail.com>
2026-04-23 12:33:56 +0300

    chore: remove dead query

--- backend/src/main/java/com/backend/matchme/repository/ConnectionRepository.java
+++ backend/src/main/java/com/backend/matchme/repository/ConnectionRepository.java
@@ -11,12 +11,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ConnectionRepository extends JpaRepository<Connection, Long> {
-    @Query("SELECT c FROM Connection c WHERE ((c.requester.id = :userAId AND c.receiver.id = :userBId) OR " +
-            "(c.requester.id = :userBId AND c.receiver.id = :userAId)) " +
-            "AND c.status = com.backend.matchme.enums.ConnectionStatus.ACCEPTED")
-    Optional<Connection> findConnection(@Param("userAId") Long userAId,
-                                        @Param("userBId") Long userBId);
-
     Optional<Connection> findByRequesterAndReceiver(User requester, User receiver);
 
     Optional<Connection> findByRequesterAndReceiverAndStatus(User requester, User receiver, ConnectionStatus status);
