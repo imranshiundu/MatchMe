24ab6e6b561abc074014e83e2a1fbf292dd40761
Lauri Musto <laurimusto@gmail.com>
2026-04-27 19:34:25 +0300

    dashboard now handles mobile and 5 in row

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -55,7 +55,7 @@ function Dashboard() {
 
                     {recommendations?.ids?.length > 0 ? (
                         <>
-                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-8">
+                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mb-8">
                                 {recommendations.ids.map((userId, index) => (
                                     <SuggestedUserCard key={`${userId}-${index}`} userID={userId}/>
                                 ))}
